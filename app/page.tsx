7525a79608e24870e40ef2a9e6494d3c72ef1af0
elgrillo83 <elgrillo83@gmail.com>
2024-03-04 13:02:32 +0100

    Install Material UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@mui/material";
+
 export default function Home() {
-  return <main>Main</main>;
+  return (
+    <main>
+      <Button variant="contained">Hello world</Button>
+    </main>
+  );
 }
