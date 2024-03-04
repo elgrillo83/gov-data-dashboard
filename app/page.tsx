f6de4dfdea43085c0977190fa9b6095c152281f2
elgrillo83 <elgrillo83@gmail.com>
2024-03-04 16:52:25 +0100

    Fetch and display organization list data

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
 import { Button } from "@mui/material";
 
-export default function Home() {
+async function getData() {
+  const response = await fetch(
+    "https://www.govdata.de/ckan/api/3/action/organization_list?all_fields=true"
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return response.json();
+}
+
+export default async function Home() {
+  const data = await getData();
+
   return (
     <main>
       <Button variant="contained">Hello world</Button>
+
+      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
     </main>
   );
 }
