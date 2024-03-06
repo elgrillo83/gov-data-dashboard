593a0d2c8274badf1426228eed9feb5279c48525
elgrillo83 <elgrillo83@gmail.com>
2024-03-06 12:01:45 +0100

    Provide organizations list from JSON file
    as the govdata.de/CKAN API is currently in maintenance mode.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,23 @@ import { Stack, Typography } from "@mui/material";
 import DepartmentsList from "../components/DepartmentsList";
 import OrganizationsTable from "../components/OrganizationsTable";
 import PackageCountsByOrganizationChart from "../components/PackageCountsByOrganizationChart";
+import organizationsListJson from "../data/organizations_list.json";
 import { Organization } from "../types";
 
 export const ORGANIZATIONS_URL =
   "https://www.govdata.de/ckan/api/3/action/organization_list?all_fields=true";
 
 async function fetchOrganizations() {
-  const response = await fetch(ORGANIZATIONS_URL);
+  // NOTE: Due to maintenance on the govdata.de/CKAN API, we are using a local JSON file
+  // const response = await fetch(ORGANIZATIONS_URL);
 
-  if (!response.ok) {
-    throw new Error("Failed to fetch data");
-  }
+  // if (!response.ok) {
+  //   throw new Error("Failed to fetch data");
+  // }
 
-  return response.json();
+  // return response.json();
+
+  return organizationsListJson;
 }
 
 export default async function Home() {
